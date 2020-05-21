c1233d37eaba28747b42962230c79921f3b21938
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-05-21 13:52:15 -0300

    Criação da página de cadastro

---
+++ cadastrar.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça agora o seu cadastro</title>
+
+    <meta charset="UTF-8">
+	<meta name="description" content="TheQuest Gaming Magazine Template">
+	<meta name="keywords" content="gaming, html">
+	
+	<!-- Favicon -->
+	<link href="img/naruto2.png" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/cadastrar.css">
+</head>
+<body>
+    	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<a href="index.html" class="site-logo">
+			<img src="img/logo.png" alt="logo">
+		</a>
+		<ul class="main-menu">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="characters.html">Sobre Nós</a></li>
+			<li><a href="game.html">Sobre o Jogo</a></li>
+			<li><a href="reviews.html">Download</a></li>
+			<!--li><a href="news.html">Forúm</a></li-->
+			<li><a href="single-post.html">Seja um Doador</a></li>
+			<!--li><a href="single-post.html">Login/Cadastro</a></li-->
+		</ul>		
+	</header>
+    <!-- Header section end -->
+    
+    <!--Seção cadastro-->
+    <section id="section-cadastro">
+        <div class="container">
+            <div class="row">  
+                <div class="col-lg-4 mb-5"><h3 align="left">Faça o seu cadastro</h3></div>
+                <div class="col-lg">             
+                    <form action="" method="POST" name="cadastrar-form">
+                        <div class="row mb-3">
+                            <div class="col-lg-6 mb-3">
+                                <label for="email">Informe seu nome</label>
+                                <input type="text" name="nome" id="nome" class="col">
+                            </div>
+
+                            <div class="col-lg-6 mb-3">                      
+                                <label for="email">Informe seu sobrenome</label> 
+                                <input type="text" name="sobrenome" id="sobrenome"class="col">
+                            </div>                       
+                        </div> 
+                        
+                        <div class="row mb-3">
+                            <div class="col-lg-6 mb-3">
+                                <label for="email">Informe seu email</label>
+                                <input type="email" name="email" id="email" class="col">
+                            </div>
+
+                            <div class="col-lg-6 mb-3">                      
+                                <label for="email">Escolha aqui seu nome de usuário</label> 
+                                <input type="text" name="username" id="username"class="col">
+                            </div>                       
+                        </div>
+                        
+                        <div class="row mb-3">
+                            <div class="col-lg-6 mb-3">
+                                <label for="email">Digite sua senha</label>
+                                <input type="password" name="senha" id="senha" class="col">
+                            </div>
+
+                            <div class="col-lg-6 mb-3">                      
+                                <label for="email">Repita a senha informada</label> 
+                                <input type="password" class="col">
+                            </div>                       
+                        </div>
+                        
+                        <button type="submit" class="btn-form">Concluido</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- Footer section -->
+<div class="footer-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="footer-widget">
+						<div class="about-widget">
+							<img src="img/logo.png" alt="">
+							<p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo. Morbi id dictum quam, ut commodo.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Usfull Links</h2>
+						<ul>
+							<li><a href="">Games</a></li>
+							<li><a href="">testimonials</a></li>
+							<li><a href="">Reviews</a></li>
+							<li><a href="">Characters</a></li>
+							<li><a href="">Latest news</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Services</h2>
+						<ul>
+							<li><a href="">About us</a></li>
+							<li><a href="">Services</a></li>
+							<li><a href="">Become a writer</a></li>
+							<li><a href="">Jobs</a></li>
+							<li><a href="">FAQ</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Careeres</h2>
+						<ul>
+							<li><a href="">Donate</a></li>
+							<li><a href="">Services</a></li>
+							<li><a href="">Subscriptions</a></li>
+							<li><a href="">Careers</a></li>
+							<li><a href="">Our team</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget fw-latest-post">
+						<h2 class="fw-title">Usfull Links</h2>
+						<div class="latest-news-widget">
+							<div class="ln-item">
+								<div class="ln-text">
+									<div class="ln-date">April 1, 2019</div>
+									<h6>10 Amazing new games</h6>
+									<div class="ln-metas">
+										<div class="ln-meta">By Admin</div>
+										<div class="ln-meta">in <a href="#">Games</a></div>
+										<div class="ln-meta">3 Comments</div>
+									</div>
+								</div>
+							</div>
+							<div class="ln-item">
+								<div class="ln-text">
+									<div class="ln-date">April 1, 2019</div>
+									<h6>10 Amazing new games</h6>
+									<div class="ln-metas">
+										<div class="ln-meta">By Admin</div>
+										<div class="ln-meta">in <a href="#">Games</a></div>
+										<div class="ln-meta">3 Comments</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright"><a href=""><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
+		</div>
+
+    <div class="social-links-warp">
+		<div class="container">
+			<div class="social-links" style="display: flex; justify-content: center;">
+				<a href="#"><i class="fab fa-instagram"></i><span>instagram</span></a>
+				<a href="#"><i class="fab fa-pinterest"></i><span>pinterest</span></a>
+				<a href="#"><i class="fab fa-facebook"></i><span>facebook</span></a>
+				<!--a href="#"><i class="fa fa-twitter"></i><span>twitter</span></a-->
+				<a href="#"><i class="fab fa-youtube"></i><span>youtube</span></a>
+				<!--a href="#"><i class="fa fa-tumblr-square"></i><span>tumblr</span></a-->
+			</div>
+		</div>
+    </div>
+    <!-- Footer section end -->
+
+    <!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
