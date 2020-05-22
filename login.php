f1191e233d5502669b23a9134c1eee89144f556a
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-05-22 11:22:10 -0300

    Criação da página de login

---
+++ login.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>World of Naruto</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="TheQuest Gaming Magazine Template">
+	<meta name="keywords" content="gaming, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="img/naruto2.png" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<a href="index.php" class="site-logo">
+			<img src="img/logo.png" alt="logo">
+		</a>
+		<ul class="main-menu">
+			<li><a href="index.html">Home</a></li>
+			<li><a href="characters.html">Sobre Nós</a></li>
+			<li><a href="game.html">Sobre o Jogo</a></li>
+			<li><a href="reviews.html">Download</a></li>
+			<!--li><a href="news.html">Forúm</a></li-->
+			<li><a href="single-post.html">Seja um Doador</a></li>
+			<!--li><a href="single-post.html">Login/Cadastro</a></li-->
+		</ul>
+	</header>
+	<!-- Header section end -->
+	
+<!-- Footer section -->
+<div class="footer-section">
+		<div class="container">
+			<p class="text-rodape">
+			  World of Naruto é uma brincadeira entre fãs de Naruto e Ragnarok, não é um produto comercial, 
+			  não tem fins comerciais nem lucrativos e está restrito a ser um passa-tempo lúdico. <br> 
+			  Copyright BORUTO: NARUTO NEXT GENERATIONS © 2002 MASASHI KISHIMOTO/ 2017 BORUTO All Right Reserved <br>
+			  Template created by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+			</p>		
+		</div>
+		<div class="social-links-warp">
+			<div class="container">
+				<div class="social-links" style="display: flex; justify-content: center;">
+					<a href="#"><i class="fab fa-instagram"></i><span>instagram</span></a>
+					<a href="#"><i class="fab fa-pinterest"></i><span>pinterest</span></a>
+					<a href="#"><i class="fab fa-facebook"></i><span>facebook</span></a>
+					<!--a href="#"><i class="fa fa-twitter"></i><span>twitter</span></a-->
+					<a href="#"><i class="fab fa-youtube"></i><span>youtube</span></a>
+					<!--a href="#"><i class="fa fa-tumblr-square"></i><span>tumblr</span></a-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer section end -->
+															
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
