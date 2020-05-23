339d286666ffb94d3f9a727514100defe62a3038
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-05-23 15:05:15 -0300

    Adição do verificador de login

---
+++ verifica_login.php
@@ -0,0 +1,20 @@
+<?php
+/*session_start();*/
+
+if(!isset($_SESSION['usuario']))
+{
+  $_SESSION['nao_autenticado'] = true;
+	/*header('Location: index.php');
+	exit();*/
+}
+
+//VERIFICADOR DE LOGIN
+/*
+//session_start();
+if(!$_SESSION['usuario'])
+{
+	header('Location: login.php');
+	exit();
+}*/
+
+//VERIFICADOR DE LOGIN
