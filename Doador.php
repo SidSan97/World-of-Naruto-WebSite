a12d4fac8697600a14c34d91f21f220e87f3e97b
asekmek <65336437+asekmek@users.noreply.github.com>
2020-05-22 22:55:29 -0300

    Add files via upload

---
+++ Doador.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>World of Naruto</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="TheQuest Gaming Magazine Template">
+	<meta name="keywords" content="gaming, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="img/naruto2.png" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<a href="index.html" class="site-logo">
+			<img src="img/logo.png" alt="logo">
+		</a>
+		<ul class="main-menu">
+			<li><a href="index.html">Home</a></li>
+			<li><a href="characters.html">Sobre Nós</a></li>
+			<li><a href="game.html">Sobre o Jogo</a></li>
+			<li><a href="reviews.html">Download</a></li>
+			<!--li><a href="news.html">Forúm</a></li-->
+			<li><a href="single-post.html">Seja um Doador</a></li>
+			<!--li><a href="single-post.html">Login/Cadastro</a></li-->
+		</ul>
+		<div class="header-add">
+			<p>Login/Cadastro</p>
+			<button><i class="fas fa-user"></i> Entrar </button>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hero-item set-bg" data-setbg="img/slider/1.jpg">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-10 offset-lg-1">
+							<h2>Escolha seu personagem</h2>
+							<p>Escolha entre os vários personagens disponiveis, cada um com caracteristicas únicas.</p>
+							<a href="#" class="site-btn">Ver Sobre</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hero-item set-bg" data-setbg="img/slider/2.png">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-10 offset-lg-1">
+							<h2>Escolha sua arena de combate</h2>
+							<p>Desfrute das diversas arenas disponiveis no jogo e parta para o combate!</p>
+							<a href="#" class="site-btn">Ver sobre</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+	<!-- Blog section -->
+	<section class="blog-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 blog-posts">
+					<div class="blog-post featured-post" style="text-align: center">
+						<img src="img/blog/1.png" alt="">
+						<h3>Ajude-nos a manter esse sonho vivo</h3>
+						<p>Word of Naruto é um jogo multiplayer feito por fãs do anime que também amam RPG e jogos MOBA. por essa questão esse fân game precisa muito do axilio de todos para cobrir os gastos do projeto e poder melhora-lo cada vez mais, onde todos os ganhos serão onvertidos para poder proporcionar uma jogatina cada vez melhor para cada jogador, como recompença por nos ajudar seram adicionados alguns itens e skills para poder melhorar ainda mais a gameplay.<br>Muito obrigado desde já em nome de toda a equipe WON.</p>
+						<br>
+						<br>
+						<a href="#" class="site-btn">Seja um doador!</a>
+						<br>
+						<br>
+						<h3>Itens e skills para doadores</h3>
+						<div class="post-metas">
+							<table class="table table-dark">
+								<thead>
+								<tr style="text-align: center">
+									<th>Itens</th>
+									<th>Skills </th>
+								</tr>
+								</thead>
+								<tbody>
+									<td>Roupa aktsuki
+										<br>
+										Roupa naruto
+										<br>
+										E etc...
+									</td>
+									<td>
+										Sharingan
+										<br>
+										8 trigramas
+										<br>
+										Byakugan
+										<br>
+										E etc...
+									</td>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-lg-4 sidebar">
+					<div class="sb-widget">
+						<form class="sb-search">
+							<div style="display: flex;">
+								<input type="text" placeholder="Search" style="width: 90%;">
+								<button class="btn-search"><i class="fas fa-search"></i></button>
+							</div>
+						</form>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Marcadores</h2>
+						<ul class="sb-cata-list">
+							<li>Multiplayer</li>
+							<li>RPG</li>
+							<li>Gratuito para Jogar</li>
+							<li>Estrategico</li>
+							<li>Competitivo</li>
+							<li>Baseado em Equipes</li>
+						</ul>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Ranking do Mês</h2>						
+						<div id="ranking-div">
+							<ul class="ranking-list">
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>							
+							</ul>
+					    </div>
+						<!--a href="#">Ver mais >></a-->
+					</div>
+					<div class="sb-widget">
+						<a href="#" class="add">
+							<img src="img/add-2.jpg" alt="">
+						</a>
+					</div>
+					<!--div class="sb-widget">
+						<h2 class="sb-title">Latest Comments</h2>
+						<div class="latest-comments-widget">
+							<div class="lc-item">
+								<img src="img/author-thumbs/1.jpg" alt="">
+								<div class="lc-text">
+									<h6>Jane Smith<span> In </span><a href="">The best 2019 Games</a></h6>
+									<div class="lc-date">April 1,2019</div>
+								</div>
+							</div>
+							<div class="lc-item">
+								<img src="img/author-thumbs/2.jpg" alt="">
+								<div class="lc-text">
+									<h6>Michael James<span> In </span><a href="">The best 2019 Games</a></h6>
+									<div class="lc-date">April 1,2019</div>
+								</div>
+							</div>
+							<div class="lc-item">
+								<img src="img/author-thumbs/3.jpg" alt="">
+								<div class="lc-text">
+									<h6>Jane Smith<span> In </span><a href="">The best 2019 Games</a></h6>
+									<div class="lc-date">April 1,2019</div>
+								</div>
+							</div>
+							<div class="lc-item">
+								<img src="img/author-thumbs/4.jpg" alt="">
+								<div class="lc-text">
+									<h6>Michael James<span> In </span><a href="">The best 2019 Games</a></h6>
+									<div class="lc-date">April 1,2019</div>
+								</div>
+							</div>
+							<div class="lc-item">
+								<img src="img/author-thumbs/1.jpg" alt="">
+								<div class="lc-text">
+									<h6>Jane Smith<span> In </span><a href="">The best 2019 Games</a></h6>
+									<div class="lc-date">April 1,2019</div>
+								</div>
+							</div>
+						</div>
+					</div-->
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog section end -->
+
+	<!-- Blog list section -->
+	<section class="galery-list-section spad">
+		<div class="container">
+			<h2 class="sb-title mb-5">Galeria</h2>
+			
+			<div class="row mb-3">
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+			</div>
+
+			<div class="row mb-3">
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+				<div class="col-lg mb-2">
+					<img src="img/galery/2.png" height="300px">
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Blog list section end -->
+
+	<!-- Video section -->
+	<div class="video-section">
+		<div class="container">
+			<div class="video-logo">
+				<img src="img/logo-2.png" alt="">
+				<p>World of Naruto Gameplay</p>
+			</div>
+			<div class="video-popup-warp">
+				<img src="img/video-bg.jpg" alt="">
+				<a href="https://www.youtube.com/watch?v=xzCEdSKMkdU" class="video-play"><i class="fa fa-play"></i></a>
+			</div>
+		</div>
+	</div>
+	<!-- Video section end -->
+
+	<!-- Footer section -->
+	<div class="footer-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="footer-widget">
+						<div class="about-widget">
+							<img src="img/logo.png" alt="">
+							<p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo. Morbi id dictum quam, ut commodo.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Usfull Links</h2>
+						<ul>
+							<li><a href="">Games</a></li>
+							<li><a href="">testimonials</a></li>
+							<li><a href="">Reviews</a></li>
+							<li><a href="">Characters</a></li>
+							<li><a href="">Latest news</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Services</h2>
+						<ul>
+							<li><a href="">About us</a></li>
+							<li><a href="">Services</a></li>
+							<li><a href="">Become a writer</a></li>
+							<li><a href="">Jobs</a></li>
+							<li><a href="">FAQ</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Careeres</h2>
+						<ul>
+							<li><a href="">Donate</a></li>
+							<li><a href="">Services</a></li>
+							<li><a href="">Subscriptions</a></li>
+							<li><a href="">Careers</a></li>
+							<li><a href="">Our team</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget fw-latest-post">
+						<h2 class="fw-title">Usfull Links</h2>
+						<div class="latest-news-widget">
+							<div class="ln-item">
+								<div class="ln-text">
+									<div class="ln-date">April 1, 2019</div>
+									<h6>10 Amazing new games</h6>
+									<div class="ln-metas">
+										<div class="ln-meta">By Admin</div>
+										<div class="ln-meta">in <a href="#">Games</a></div>
+										<div class="ln-meta">3 Comments</div>
+									</div>
+								</div>
+							</div>
+							<div class="ln-item">
+								<div class="ln-text">
+									<div class="ln-date">April 1, 2019</div>
+									<h6>10 Amazing new games</h6>
+									<div class="ln-metas">
+										<div class="ln-meta">By Admin</div>
+										<div class="ln-meta">in <a href="#">Games</a></div>
+										<div class="ln-meta">3 Comments</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright"><a href=""><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
+		</div>
+		<div class="social-links-warp">
+			<div class="container">
+				<div class="social-links" style="display: flex; justify-content: center;">
+					<a href="#"><i class="fab fa-instagram"></i><span>instagram</span></a>
+					<a href="#"><i class="fab fa-pinterest"></i><span>pinterest</span></a>
+					<a href="#"><i class="fab fa-facebook"></i><span>facebook</span></a>
+					<!--a href="#"><i class="fa fa-twitter"></i><span>twitter</span></a-->
+					<a href="#"><i class="fab fa-youtube"></i><span>youtube</span></a>
+					<!--a href="#"><i class="fa fa-tumblr-square"></i><span>tumblr</span></a-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer section end -->
+															
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ Download.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>World of Naruto</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="TheQuest Gaming Magazine Template">
+	<meta name="keywords" content="gaming, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="img/naruto2.png" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<a href="index.html" class="site-logo">
+			<img src="img/logo.png" alt="logo">
+		</a>
+		<ul class="main-menu">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="characters.html">Sobre Nós</a></li>
+			<li><a href="game.html">Sobre o Jogo</a></li>
+			<li><a href="Download.php">Download</a></li>
+			<!--li><a href="news.html">Forúm</a></li-->
+			<li><a href="single-post.html">Seja um Doador</a></li>
+			<!--li><a href="single-post.html">Login/Cadastro</a></li-->
+		</ul>
+		<div class="header-add">
+			<p>Login/Cadastro</p>
+			<button><i class="fas fa-user"></i> Entrar </button>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hero-item set-bg" data-setbg="img/slider/1.jpg">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-10 offset-lg-1">
+							<h2>Escolha seu personagem</h2>
+							<p>Escolha entre os vários personagens disponiveis, cada um com caracteristicas únicas.</p>
+							<a href="#" class="site-btn">Ver Sobre</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hero-item set-bg" data-setbg="img/slider/2.png">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-10 offset-lg-1">
+							<h2>Escolha sua arena de combate</h2>
+							<p>Desfrute das diversas arenas disponiveis no jogo e parta para o combate!</p>
+							<a href="#" class="site-btn">Ver sobre</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+	<!-- Blog section -->
+	<section class="blog-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 blog-posts">
+					<div class="blog-post featured-post" style="text-align: center">
+						<a href="#" class="site-btn">Download game</a>
+						<br>
+						<br>
+						<div class="post-metas">
+							<table class="table table-dark">
+								<thead>
+								<tr style="text-align: center">
+									<th>Requisitos Minimos</th>
+									<th>Requisitos recomendados</th>
+								</tr>
+								</thead>
+								<tbody>
+									<td>
+										Sistema Operacional. Windows 98SE, ME, 2000 ou XP. Windows Vista, 7, 8 ou 10.
+										<br>
+										Processador. Intel/AMD 1 GHz. Intel/AMD 1.8 GHz ou superior.
+										<br>
+										Memória RAM. 2 GB ou mais.
+										<br>
+										Placa de vídeo. Placa de vídeo de 64MB. Placa de vídeo de 128MB.
+										<br>
+										Versão do DirectX. 8.0 ou superior.
+										<br>
+										Espaço em disco. 3 GB de espaço em disco
+									</td>
+										<td>
+										Sistema Operacional. Windows, 2000 ou XP. Windows Vista, 7, 8 ou 10.
+										<br>
+										Processador. Intel/AMD 1 GHz. Intel/AMD 1.8 GHz ou superior.
+										<br>
+										Memória RAM. 4 GB ou mais.
+										<br>
+										Placa de vídeo. Placa de vídeo de 128MB.
+										<br>
+										Versão do DirectX. 8.0 ou superior.
+										<br>
+										Espaço em disco. 3 GB de espaço em disco
+									</td>
+
+								</tbody>
+							</table>
+						</div>
+						<p>Word of Naruto é um verão adaptada, onde o mesmo so pode ser executado em verções apenas do windows sendo assim não possui compactibilidade com o sistema operacional linux</p>
+
+						<div class="video-section">
+							<div class="container">
+								<div class="video-logo">
+								<p>Tutorial como realizar a instalação executando corretamente o jogo e também como criar o personagem com a conta criada no site</p>
+								</div>
+								<div class="video-popup-warp">
+									<img src="img/video-bg.jpg" alt="">
+									<a href="https://www.youtube.com/watch?v=xzCEdSKMkdU" class="video-play"><i class="fa fa-play"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 sidebar">
+					<div class="sb-widget">
+						<form class="sb-search">
+							<div style="display: flex;">
+								<input type="text" placeholder="Search" style="width: 90%;">
+								<button class="btn-search"><i class="fas fa-search"></i></button>
+							</div>
+						</form>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Marcadores</h2>
+						<ul class="sb-cata-list">
+							<li>Multiplayer</li>
+							<li>RPG</li>
+							<li>Gratuito para Jogar</li>
+							<li>Estrategico</li>
+							<li>Competitivo</li>
+							<li>Baseado em Equipes</li>
+						</ul>
+					</div>
+					<div class="sb-widget">
+						<h2 class="sb-title">Ranking do Mês</h2>						
+						<div id="ranking-div">
+							<ul class="ranking-list">
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>
+								<li><span class="col">1º Player1</span> <span class="col offset-4">2020 pontos</span></li>							
+							</ul>
+					    </div>
+						<!--a href="#">Ver mais >></a-->
+					</div>
+					<div class="sb-widget">
+						<a href="#" class="add">
+							<img src="img/add-2.jpg" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Video section -->
+	<div class="video-section">
+		<div class="container">
+			<div class="video-logo">
+				<img src="img/logo-2.png" alt="">
+				<p>World of Naruto Gameplay</p>
+			</div>
+			<div class="video-popup-warp">
+				<img src="img/video-bg.jpg" alt="">
+				<a href="https://www.youtube.com/watch?v=xzCEdSKMkdU" class="video-play"><i class="fa fa-play"></i></a>
+			</div>
+		</div>
+	</div>
+	<!-- Video section end -->
+
+	<!-- Footer section -->
+	<div class="footer-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="footer-widget">
+						<div class="about-widget">
+							<img src="img/logo.png" alt="">
+							<p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo. Morbi id dictum quam, ut commodo.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Usfull Links</h2>
+						<ul>
+							<li><a href="">Games</a></li>
+							<li><a href="">testimonials</a></li>
+							<li><a href="">Reviews</a></li>
+							<li><a href="">Characters</a></li>
+							<li><a href="">Latest news</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Services</h2>
+						<ul>
+							<li><a href="">About us</a></li>
+							<li><a href="">Services</a></li>
+							<li><a href="">Become a writer</a></li>
+							<li><a href="">Jobs</a></li>
+							<li><a href="">FAQ</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-sm-6">
+					<div class="footer-widget">
+						<h2 class="fw-title">Careeres</h2>
+						<ul>
+							<li><a href="">Donate</a></li>
+							<li><a href="">Services</a></li>
+							<li><a href="">Subscriptions</a></li>
+							<li><a href="">Careers</a></li>
+							<li><a href="">Our team</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget fw-latest-post">
+						<h2 class="fw-title">Usfull Links</h2>
+						<div class="latest-news-widget">
+							<div class="ln-item">
+								<div class="ln-text">
+									<div class="ln-date">April 1, 2019</div>
+									<h6>10 Amazing new games</h6>
+									<div class="ln-metas">
+										<div class="ln-meta">By Admin</div>
+										<div class="ln-meta">in <a href="#">Games</a></div>
+										<div class="ln-meta">3 Comments</div>
+									</div>
+								</div>
+							</div>
+							<div class="ln-item">
+								<div class="ln-text">
+									<div class="ln-date">April 1, 2019</div>
+									<h6>10 Amazing new games</h6>
+									<div class="ln-metas">
+										<div class="ln-meta">By Admin</div>
+										<div class="ln-meta">in <a href="#">Games</a></div>
+										<div class="ln-meta">3 Comments</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright"><a href=""><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
+		</div>
+		<div class="social-links-warp">
+			<div class="container">
+				<div class="social-links" style="display: flex; justify-content: center;">
+					<a href="#"><i class="fab fa-instagram"></i><span>instagram</span></a>
+					<a href="#"><i class="fab fa-pinterest"></i><span>pinterest</span></a>
+					<a href="#"><i class="fab fa-facebook"></i><span>facebook</span></a>
+					<!--a href="#"><i class="fa fa-twitter"></i><span>twitter</span></a-->
+					<a href="#"><i class="fab fa-youtube"></i><span>youtube</span></a>
+					<!--a href="#"><i class="fa fa-tumblr-square"></i><span>tumblr</span></a-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Footer section end -->
+															
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
